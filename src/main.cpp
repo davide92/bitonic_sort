86918d5e0422c3bbbb7eece12e0a22af84391cea
davide92 <thechryst92@gmail.com>
2019-04-11 17:57:16 +0200

    Added the input arguments parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <stdexcept>
+#include <string>
 #include <sys/time.h>
 #include "utils.h"
 
@@ -24,14 +27,39 @@ int main(int argc , char** argv) {
     double endTime ;
 
     /*Check if enough arguments were passed to the process.*/
-    if ( argc < 3) {
+    if (argc < 3) {
         /*If not print a warning message with the correct way to use*/
         /*the program and terminate the execution. */
         printf("Invalid command line argument option! \n");
-        printf("Usage : %s p q where p is the number of MPI processes to "
-               "be spawned and q the number of elements to sort. \n ",
-               argv[0] );
+        printf("Usage : %s p q where p is the number of MPI processes to be spawned and q the number of elements to sort. \n ", argv[0]);
         exit(ARG_ERROR);
+    }
+
+    /* Parse the command line arguments. */
+    std::string arg1 = argv[1];
+    try {
+        std::size_t pos;
+        p = std::stoi(arg1, &pos);
+        if (pos < arg1.size()) {
+            std::cerr << "Trailing characters after number: " << arg1 << '\n';
+        }
+    } catch (std::invalid_argument const &ex) {
+        std::cerr << "Invalid number: " << arg1 << '\n';
+    } catch (std::out_of_range const &ex) {
+        std::cerr << "Number out of range: " << arg1 << '\n';
+    }
+
+    std::string arg2 = argv[2];
+    try {
+        std::size_t pos;
+        q = std::stoi(arg2, &pos);
+        if (pos < arg2.size()) {
+            std::cerr << "Trailing characters after number: " << arg2 << '\n';
+        }
+    } catch (std::invalid_argument const &ex) {
+        std::cerr << "Invalid number: " << arg2 << '\n';
+    } catch (std::out_of_range const &ex) {
+        std::cerr << "Number out of range: " << arg2 << '\n';
     }
 
 }
