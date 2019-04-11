b11c91dca3e6b4e93f8ba369b45fa9185e3e7d4f
davide92 <thechryst92@gmail.com>
2019-04-11 17:47:35 +0200

    Added the input arguments checking

--- include/utils.h
+++ include/utils.h
@@ -1,4 +1,13 @@
 #ifndef BITONIC_SORT_UTILS_H
 #define BITONIC_SORT_UTILS_H
 
+#include <stdlib.h>
+
+enum {
+    ARG_ERROR = 3,
+    MALLOC_ERROR,
+    NTHREADS_ERROR
+} errors;
+
+
 #endif //BITONIC_SORT_UTILS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include "utils.h"
 
 int main(int argc , char** argv) {
+
+    /*Processor ID*/
+    int processID;
+
+    /*Local array of elements for each processor*/
+    int array;
+
+    /*Number of elements in the array*/
+    int n_array;
+
+    /*Number of tasks running*/
+    int num_tasks;
+
+    /*Number of processor and number of elements to sort passed as input arguments*/
+    int p, q;
+
+    double startTime;
+    double endTime ;
+
+    /*Check if enough arguments were passed to the process.*/
+    if ( argc < 3) {
+        /*If not print a warning message with the correct way to use*/
+        /*the program and terminate the execution. */
+        printf("Invalid command line argument option! \n");
+        printf("Usage : %s p q where p is the number of MPI processes to "
+               "be spawned and q the number of elements to sort. \n ",
+               argv[0] );
+        exit(ARG_ERROR);
+    }
 
 }
